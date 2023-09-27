e469444cadf74db90e51319ae92c16e2aace395f
Aditya Upadhyay <adityaupadhyay.1311@gmail.com>
2023-09-27 16:23:48 -0400

    Login Component created

--- front-end/src/app/app-routing.module.ts
+++ front-end/src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'authentication',
+    pathMatch: 'full'
+  },
+  {
+    path: 'authentication',
+    loadChildren: () => import('./modules/authentication/authentication.module').then(auth => auth.AuthenticationModule )
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ front-end/src/app/modules/authentication/authentication.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { LoginComponent } from './login/login.component';
+import { AuthenticationRoutingModule } from './authentication.routing';
+
+
+
+@NgModule({
+  declarations: [
+    LoginComponent
+  ],
+  imports: [
+    CommonModule,
+    AuthenticationRoutingModule
+  ]
+})
+export class AuthenticationModule { }
