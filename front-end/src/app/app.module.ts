50265fa44fbdd00392beaa743893b6dbe55a42f7
Aditya Upadhyay <adityaupadhyay.1311@gmail.com>
2023-09-27 18:07:33 -0400

    Login screen added

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -1,8 +1,10 @@
-import { NgModule } from '@angular/core';
+import {NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
   declarations: [
@@ -10,7 +12,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    CommonModule,
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- front-end/src/app/modules/authentication/login/login.component.ts
+++ front-end/src/app/modules/authentication/login/login.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import {DefaultButtonComponent} from '../../../components/default-button/default-button.component';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit  {
+  loginForm!:FormGroup;
+  isSubmitted = false;
+  returnUrl = '';
+  constructor(private formBuilder: FormBuilder,
+    // , private userService:UserService,
+     private activatedRoute:ActivatedRoute,
+     private router:Router) { }
+
+  ngOnInit(): void {
+    this.loginForm = this.formBuilder.group({
+      email:['', [Validators.required,Validators.email]],
+      password:['', Validators.required]
+    });
+
+    this.returnUrl = this.activatedRoute.snapshot.queryParams['returnUrl'];
+  }
+
+  get fc(){
+    return this.loginForm.controls;
+  }
+
+  submit(){
+    this.isSubmitted = true;
+    if(this.loginForm.invalid) return;
+
+    // this.userService.login({email:this.fc.email.value,
+    //    password: this.fc.password.value}).subscribe(() => {
+    //      this.router.navigateByUrl(this.returnUrl);
+    //    });
+  }
 
 }
